261d1646ad625338673201ff220f2deb47fb2494
Mihriban Alp <mihribanalp.05@gmail.com>
2021-04-15 18:04:21 +0300

    add social accounts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import "./mysass.scss";
+import "./style.scss";
 import { Container, Row, Col } from "react-bootstrap";
-import Projects from "./contents/projects/Projects";
-import Blogs from "./contents/blogs/Blogs";
-import Alien from "./components/icons/Alien";
+import Blog from "./components/blog/Blog";
+import Project from "./components/project/Project";
 import * as Icons from "./components/icons";
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
       </Row>
       <Row className="about">
         <Col>
-          <p className="title">Hey there, I'm Mihriban {<Alien />}</p>
+          <p className="title">Hey there, I'm Mihriban {<Icons.Alien />}</p>
           <p>
             I'm a student of Computer Engineering. Lorem ipsum dolor sit amet,
             consectetur adipisicing elit. Amet cum temporibus fugiat culpa unde
@@ -29,24 +28,54 @@ function App() {
         </Col>
       </Row>
       <Row className="blogs">
-        <Blogs />
+        <Col>
+          <Row>
+            <Col className="title">
+              <p>Latest Posts</p>
+            </Col>
+          </Row>
+          <Row>
+            <Blog />
+          </Row>
+          <Row>
+            <Blog />
+          </Row>
+        </Col>
       </Row>
       <Row className="projects">
-        <Projects />
+        <Col>
+          <Row className="title">
+            <Col>
+              <p>Best Projects</p>
+            </Col>
+          </Row>
+          <Row>
+            <Project
+              projectName="Scientific Calculator"
+              linkUrl="https://github.com/mihrilp/scientific-calculator"
+            />
+          </Row>
+          <Row>
+            <Project
+              projectName="To-do App"
+              linkUrl="https://github.com/mihrilp/react-todoapp"
+            />
+          </Row>
+        </Col>
       </Row>
       <Row className="socialIcons">
         <Col></Col>
         <Col>
-          <Icons.Twitter />
+          <Icons.Github className="socialIcon" />
         </Col>
         <Col>
-          <Icons.Github />
+          <Icons.Twitter className="socialIcon" />
         </Col>
         <Col>
-          <Icons.Linkedin />
+          <Icons.Linkedin className="socialIcon" />
         </Col>
         <Col>
-          <Icons.Spotify />
+          <Icons.Spotify className="socialIcon" />
         </Col>
         <Col></Col>
       </Row>
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,10 +1,6 @@
-export { default as Discord } from "./Discord";
-export { default as Facebook } from "./Facebook";
 export { default as Github } from "./Github";
-export { default as Instagram } from "./Instagram";
 export { default as Linkedin } from "./Linkedin";
 export { default as Spotify } from "./Spotify";
-export { default as Telegram } from "./Telegram";
 export { default as Twitter } from "./Twitter";
 export { default as Alien } from "./Alien";
 export { default as Arrow } from "./Arrow";
