355693b7dc6fea384c3863a7452c8bc844567337
Mihriban Alp <mihrilp@gmail.com>
2021-10-27 15:53:32 +0300

    migrate to next.js

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </Layout>
+  );
+}
